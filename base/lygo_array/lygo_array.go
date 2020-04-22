4d87c588c509f361999660858883db76532aa8fa
gian angelo geminiani <angelo.geminiani@gmail.com>
2020-04-22 08:05:10 +0200

    no message

---
+++ base/lygo_array/lygo_array.go
@@ -0,0 +1,79 @@
+package lygo_array
+
+//----------------------------------------------------------------------------------------------------------------------
+//	p u b l i c
+//----------------------------------------------------------------------------------------------------------------------
+
+// https://github.com/golang/go/wiki/SliceTricks
+
+func Reverse(array interface{}) {
+	if a, b := array.([]interface{}); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]string); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]byte); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]int); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]int8); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]int16); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]int32); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]int64); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uint); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uint8); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uint16); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uint32); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uint64); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]uintptr); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]float32); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	} else if a, b := array.([]float64); b {
+		for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+			a[left], a[right] = a[right], a[left]
+		}
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+//	p r i v a t e
+//----------------------------------------------------------------------------------------------------------------------
