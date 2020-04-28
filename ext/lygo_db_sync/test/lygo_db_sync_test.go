0e4d6eac0476308eead3efb70ef97d62a00d9793
gian angelo geminiani <angelo.geminiani@gmail.com>
2020-04-28 10:55:26 +0200

    no message

--- ext/lygo_db_sync/test/lygo_db_sync_test.go
+++ ext/lygo_db_sync/test/lygo_db_sync_test.go
@@ -1,28 +1,33 @@
 package test
 
 import (
+	"github.com/botikasm/lygo/base/lygo_io"
 	"github.com/botikasm/lygo/ext/lygo_db_sync"
 	"testing"
 )
 
 func TestSimple(t *testing.T) {
 
+	psw, _ := lygo_io.ReadTextFromFile("./psw.txt")
+
 	// master
-	mc := new (lygo_db_sync.DBSyncConfig)
+	mc := new(lygo_db_sync.DBSyncConfig)
 	mc.Load("./master.json")
+	mc.Database.Authentication.Password = psw
 	master := lygo_db_sync.NewDBSyncMaster(mc)
 	err := master.Open()
-	if nil!=err{
+	if nil != err {
 		t.Error(err)
 		t.FailNow()
 	}
 
 	// slave
-	sc := new (lygo_db_sync.DBSyncConfig)
+	sc := new(lygo_db_sync.DBSyncConfig)
 	sc.Load("./slave.json")
+	sc.Database.Authentication.Password = psw
 	slave := lygo_db_sync.NewDBSyncSlave(sc)
 	err = slave.Open()
-	if nil!=err{
+	if nil != err {
 		t.Error(err)
 		t.FailNow()
 	}
